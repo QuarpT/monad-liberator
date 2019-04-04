8c61ad96643a1f84a21922df54cfb906d5615dc5
Peter Colley <zvvvvt@gmail.com>
2019-04-04 19:40:24 +0100

    it works

--- src/main/scala/MonadPrecedence.scala
+++ src/main/scala/MonadPrecedence.scala
@@ -25,35 +25,34 @@ trait PrecedenceGreaterThan[A, B]
 
 trait HListGreaterThan[A, B]
 
-//trait Unused[A]
-
-case class Unused[A](a: A)
-
-trait MegaMapT[A[_], B] {
+trait DeepMap[A[_], B] {
   def apply[C](a: A[B])(f: B => C): A[C]
 }
 
-trait MegaMap1[F[_], B] extends MegaMapT[F, B]
-
-trait MegaMap2[A, B] {
-  type Aux = B
-  def apply(a: A): B
-}
-
-object MegaMap2 {
-  def apply[A, B](a: A)(implicit mm: MegaMap2[A, B]): mm.Aux = mm(a)
-}
-
-object MegaMap1 {
-  def apply[F[_], G[_], B, C](a: F[G[B]])(f: B => C)(implicit mm: MegaMap1[Lambda[X => F[G[X]]], B]) = mm(a)(f)
-//  def apply2[F[_], G[_], B, C](a: F[G[B]])(f: B => C)(implicit mm: MegaMap1[Lambda[X => F[G[X]]], B]) = mm(a)(f)
-}
-
-//trait MegaMap2[F[_], G[_], B] extends MegaMapT[F[_ <: G], B]
-
-
-object MegaMapT {
-  def apply[A[_], B, C](a: A[B])(f: B => C)(implicit mm: MegaMapT[A,B]): A[C] = mm(a)(f)
+object DeepMap {
+  def apply[F[_], G[_], A, B](a: F[G[A]])(f: A => B)(implicit mm: DeepMap[Lambda[X => F[G[X]]], A]): F[G[B]] = mm(a)(f)
+  def apply2[F1[_], F2[_], F3[_], B, C](a: F1[F2[F3[B]]])(f: B => C)(implicit mm: DeepMap[Lambda[X => F1[F2[F3[X]]]], B]): F1[F2[F3[C]]] = mm(a)(f)
+  implicit class DeepMap1[F1[_], A](functor: F1[A])(implicit mm: DeepMap[Lambda[X => F1[X]], A]) {
+    def mp = this
+    def deepMap[B](f: A => B): F1[B] = mm(functor)(f)
+    def deepFlatMap[B, C](f: A => B)(implicit mp: MonadP[F1[B], C]): C = mp(mm(functor)(f))
+    def flatMap[B, C](f: A => B)(implicit mp: MonadP[F1[B], C]): C = deepFlatMap(f)
+    def map[B, C](f: A => B)(implicit mp: MonadP[F1[B], C]): C = deepFlatMap(f)
+  }
+  implicit class DeepMap2[F1[_], F2[_], A](functor: F1[F2[A]])(implicit mm: DeepMap[Lambda[X => F1[F2[X]]], A]) {
+    def mp = this
+    def deepMap[B](f: A => B): F1[F2[B]] = mm(functor)(f)
+    def deepFlatMap[B, C](f: A => B)(implicit mp: MonadP[F1[F2[B]], C]): C = mp(mm(functor)(f))
+    def flatMap[B, C](f: A => B)(implicit mp: MonadP[F1[F2[B]], C]): C = deepFlatMap(f)
+    def map[B, C](f: A => B)(implicit mp: MonadP[F1[F2[B]], C]): C = deepFlatMap(f)
+  }
+  implicit class DeepMap3[F1[_], F2[_], F3[_], A](functor: F1[F2[F3[A]]])(implicit mm: DeepMap[Lambda[X => F1[F2[F3[X]]]], A]) {
+    def mp = this
+    def deepMap[B](f: A => B): F1[F2[F3[B]]] = mm(functor)(f)
+    def deepFlatMap[B, C](f: A => B)(implicit mp: MonadP[F1[F2[F3[B]]], C]): C = mp(mm(functor)(f))
+    def flatMap[B, C](f: A => B)(implicit mp: MonadP[F1[F2[F3[B]]], C]): C = deepFlatMap(f)
+    def map[B, C](f: A => B)(implicit mp: MonadP[F1[F2[F3[B]]], C]): C = deepFlatMap(f)
+  }
 }
 
 trait MonadP[A, B] {
@@ -71,17 +70,10 @@ trait LowPriorityImplicits {
   implicit def identityPrecedence[A] = new MonadP[A, A] {
     override def apply(a: A): A = a
   }
+
   implicit def hListGtEvidence[A, B <: HList, C, D <: HList](implicit ev1: HListGreaterThan[B, D]): HListGreaterThan[A :: B, C :: D] = {
     new HListGreaterThan[A :: B, C :: D] {}
   }
-
-  implicit def megaMapRule2Base[F[_] : Functor, A] = new MegaMap2[F[A], Functor[F]] {
-    override def apply(a: F[A]): Functor[F] = Functor[F]
-  }
-
-//  implicit def megaMapBase[F[_] : Functor, A] = new MegaMap[F, A] {
-//    override def apply[B](functor: F[A])(f: A => B): F[B] = Functor[F].map(functor)(f)
-//  }
 
 }
 
@@ -97,33 +89,27 @@ trait MediumPriorityImplicits extends LowPriorityImplicits {
       }
     }
 
+
+
 }
 
 trait HighPriorityImplicits extends MediumPriorityImplicits {
 
-//  implicit def megaMapRule[F[_] : Functor, B] = new MegaMap1[F, B]  {
-//    override def apply[C](a: F[B])(f: B => C): F[C] = Functor[F].map(a)(f)
-//  }
-
-  implicit def megaMapRule1[F[_] : Functor, G[_] : Functor, B] = new MegaMap1[Lambda[X => F[G[X]]], B]  {
+  implicit def deepMapRule1[F[_] : Functor, B] = new DeepMap[Lambda[X => F[X]], B] {
+    override def apply[C](a: F[B])(f: B => C): F[C] = {
+      Functor[F].map(a)(f)
+    }
+  }
+  implicit def deepMapRule2[F[_] : Functor, G[_] : Functor, B] = new DeepMap[Lambda[X => F[G[X]]], B] {
     override def apply[C](a: F[G[B]])(f: B => C): F[G[C]] = {
       Functor[F].map(a)(_.map(f))
     }
   }
-
-
-  implicit def megaMapRule2Rule[F[_] : Functor, G[_], B, C[_]: Functor](implicit mm: MegaMap2[G[B], Functor[C]]) =
-    new MegaMap2[F[G[B]], Functor[Lambda[α => F[C[α]]]]] {
-      override def apply(a: F[G[B]])= {
-         Functor[F].compose[C]
-      }
+  implicit def deepMapRule3[F1[_] : Functor, F2[_] : Functor, F3[_] : Functor, B] = new DeepMap[Lambda[X => F1[F2[F3[X]]]], B] {
+    override def apply[C](a: F1[F2[F3[B]]])(f: B => C): F1[F2[F3[C]]] = {
+      Functor[F1].compose[F2].compose[F3].map(a)(f)
     }
-
-//  implicit def megaMapRule2[F[_] : Functor, G[_] : Functor, B[_]] = new MegaMap1[Lambda[X => F[G[X]]], B]  {
-//    override def apply[C](a: F[G[B]])(f: B => C): F[G[C]] = {
-//      Functor[F].map(a)(_.map(f))
-//    }
-//  }
+  }
 
   implicit def flattenPrecedence[F[_] : Monad, A, B <: HList, C, D](implicit
     precedenceEvidence: Precedence[F[_] :: B],
@@ -135,6 +121,7 @@ trait HighPriorityImplicits extends MediumPriorityImplicits {
   }
 
   implicit def precedenceTail[A, B, C](implicit precedence: Precedence[A :: B :: C :: HNil]) = new Precedence[B :: C :: HNil] {}
+
   implicit def precedenceTail2[A, B, C](implicit precedence: Precedence[A :: B :: C :: HNil]) = new Precedence[C :: HNil] {}
 
   implicit def hListBaseGtEvidence[A, B <: HList]: HListGreaterThan[A :: B, HNil] = {
@@ -152,32 +139,30 @@ trait HighPriorityImplicits extends MediumPriorityImplicits {
 
 
 object Main extends App with HighPriorityImplicits {
-import scala.reflect.runtime.universe.reify
-import scala.reflect.runtime.universe._
-    implicit val precedence = new Precedence[Future[_] :: List[_] :: Option[_] :: HNil] {}
-    val a: Option[Future[Option[Future[List[List[Future[Int]]]]]]] = Option(Future(Option(Future(List(List(Future(5)))))))
-    val z: Future[List[Option[Int]]] = MonadP(a)
+ import DeepMap._
+  import scala.reflect.runtime.universe.reify
+  import scala.reflect.runtime.universe._
 
-//  val x = MegaMapT(List(List(5)))(s => s)
+  implicit val precedence = new Precedence[Future[_] :: List[_] :: Option[_] :: HNil] {}
+  val a: Option[Future[Option[Future[List[List[Future[Int]]]]]]] = Option(Future(Option(Future(List(List(Future(5)))))))
+  val z: Future[List[Option[Int]]] = MonadP(a)
 
-//  type LL[E] = List[List[E]]
-//  implicit val  zzz: List[List[Int]] = implicitly[MegaMap1[LL, Int]].apply(List(List(5)))(x => x)
-  println(MegaMap1(List(Option(5)))(x=>x*2))
-  val aa = MegaMap1(List(Option(5)))(x=> x.toString)
-  val zz: MegaMap2[List[Int], Functor[List]] = megaMapRule2Base(Functor[List])
-  val xxxx: Seq[Int] = MegaMap2.apply(List(1)).map(List(1))(x => x + 1)
+  println(DeepMap(List(Option(5)))(x => x * 2))
+  val aa: Seq[Option[String]] = DeepMap.apply(List(Option(5)))(x => x.toString)
 
-//  val adfs = megaMapRule2Rule(Functor[List], Functor[List], zz)(List(List(1))).map(List(List(1)))(x => x)
-  val adsfs = megaMapRule2Rule(Functor[List], Functor[List], zz)
+//  val e: Either[Nothing, List[Option[Int]]] = Right(List(Option(5)))
+//  val bb: Either[Nothing, List[Option[String]]] = DeepMap.apply2(e)(_.toString)
+//  Functor[Either[String, _]]
 
-//  val xx2xx = MegaMap2.apply(List(List(1)))(adsfs).map(List(List(1)))(x => x + 1)
-//  MegaMap2(List(List(1))).map(List(List(1)))(x=>x+1)
-//  MegaMap2(List(List(1))).map(List(List(1)))(x=>x + 1)
-//
-//  adsfs(List(List(1))).map(List(List(1)))(x => x + 1)
-//  println(MegaMap2(List(List(5)))(x => x))
-//    megaMapSplitterRule[Int]
+  val zz: Future[List[Option[Int]]] = for {
+    a <- Option(List(1,2,3,4)).mp
+    b <- Option(Future(5)).mp
+    c <- Future(b * a).mp
+  } yield c
 
-//    val x = implicitly[MegaMap[Unused, List, Int]].apply()()
-//    println(Await.result(z, 5 seconds))
+  println(Await.result(zz, 5 seconds))
+
+//  val e = Option(Option(List(5))).deepFlatMap(_ * 5)
+//  println(e)
+
 }
