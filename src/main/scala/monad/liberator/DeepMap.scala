bf29c8da2e8a8af16df7ccf502773e4a57f8209a
Peter Colley <zvvvvt@gmail.com>
2019-04-06 19:35:42 +0100

    put in some information on the attempts as comments

--- src/main/scala/monad/liberator/DeepMap.scala
+++ src/main/scala/monad/liberator/DeepMap.scala
@@ -1,9 +1,37 @@
 package monad.liberator
 
-import cats.Functor
-import monadp.DeepFlatMap
-
 import scala.language.higherKinds
+
+/*
+ *
+ * I was unable to find a nice solution for deepMap. I feel I came close with:
+ *
+ *     trait DeepMap[A, B] {
+ *       type Aux = B
+ *       def apply(a: A): B
+ *     }
+ *
+ *     implicit def deepMapBase[F[_] : Functor, A] = new DeepMap[F[A], Functor[F]] {
+ *       override def apply(a: F[A]): Functor[F] = Functor[F]
+ *     }
+ *
+ *     implicit def deepMapRule[F[_] : Functor, G[_], B, C[_]: Functor](implicit mm: DeepMap[G[B], Functor[C]]) =
+ *       new DeepMap[F[G[B]], Functor[Lambda[α => F[C[α]]]]] {
+ *         override def apply(a: F[G[B]]): Functor[Lambda[α => F[C[α]]]] = Functor[F].compose[C]
+ *       }
+ *
+ * But sadly the Scala implicit heuristics wouldn't allow me to generate my a deep functor.
+ * I still think it might be possible to do this another way.
+ *
+ * As a workaround I have generated this code using:
+ * monad-precedence/src/test/scala/monad/liberator/util/GenerateDeepMonadCode.scala
+ * monad-precedence/src/test/scala/monad/liberator/util/GenerateDeepMapCode.scala
+ *
+ * I would like to improve this.
+ *
+ * Peter Colley
+ *
+ */
 
 trait DeepMap[A[_], B] {
   def apply[C](a: A[B])(f: B => C): A[C]
@@ -11,11 +39,6 @@ trait DeepMap[A[_], B] {
 
 trait DeepFlatMapImplicits {
 
-  implicit def deepMapRule1[F[_] : Functor, B] = new DeepFlatMap[Lambda[X => F[X]], B] {
-    override def apply[C](a: F[B])(f: B => C): F[C] = {
-      Functor[F].map(a)(f)
-    }
-  }
 
 
 }
--- src/main/scala/monad/liberator/DeepMonad.scala
+++ src/main/scala/monad/liberator/DeepMonad.scala
@@ -2,6 +2,35 @@ package monad.liberator
 
 import scala.language.higherKinds
 
+/*
+ *
+ * I was unable to find a nice solution for deepMap. I feel I came close with:
+ *
+ *     trait DeepMap[A, B] {
+ *       type Aux = B
+ *       def apply(a: A): B
+ *     }
+ *
+ *     implicit def deepMapBase[F[_] : Functor, A] = new DeepMap[F[A], Functor[F]] {
+ *       override def apply(a: F[A]): Functor[F] = Functor[F]
+ *     }
+ *
+ *     implicit def deepMapRule[F[_] : Functor, G[_], B, C[_]: Functor](implicit mm: DeepMap[G[B], Functor[C]]) =
+ *       new DeepMap[F[G[B]], Functor[Lambda[α => F[C[α]]]]] {
+ *         override def apply(a: F[G[B]]): Functor[Lambda[α => F[C[α]]]] = Functor[F].compose[C]
+ *       }
+ *
+ * But sadly the Scala implicit heuristics didn't seem to allow generating a deep functor. I still think it might be possible.
+ *
+ * As a workaround I have generated this code using:
+ * monad-precedence/src/test/scala/monad/liberator/util/GenerateDeepMonadCode.scala
+ * monad-precedence/src/test/scala/monad/liberator/util/GenerateDeepMapCode.scala
+ *
+ * I would like to improve this.
+ *
+ * Peter Colley
+ *
+ */
 trait DeepMonadImplicits {
     implicit class DeepMonad1[F1[_], A](functor: F1[A])(implicit dfm: DeepMap[Lambda[X => F1[X]], A]) {
     def !? = this
